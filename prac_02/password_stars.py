85c5f2e35445a497b3086e7f61e6076ee17e8442
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-09-23 12:56:29 +1000

    Add docstrings to each function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,12 @@
 def main():
+    """Print stars based on the length of a password."""
     min_length = 8
-    password = get_password(min_length)
+    password = get_valid_password(min_length)
     print('*' * len(password))
 
 
-def get_password(min_length):
+def get_valid_password(min_length):
+    """Get a password from user and check if it is valid"""
     password = input("Enter a password: ")
     while len(password) < min_length:
         print(f"Password must be at least {min_length} characters long.")
