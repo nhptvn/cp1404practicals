76a60b698f5e9f03b2c5201890bb75364b3f2a4e
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-10-14 16:28:46 +1000

    Add docstrings to function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,12 +7,14 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
+    """Read information from file and print information."""
     records = get_record_from_file(FILENAME)
     champion_to_win, countries = process_records(records)
     print_results(champion_to_win, countries)
 
 
 def print_results(champion_to_win, countries):
+    """Print the champions and countries."""
     for name, win in champion_to_win.items():
         print(name, win)
     print(f"These {len(countries)} countries have won Wimbledon:")
@@ -20,6 +22,7 @@ def print_results(champion_to_win, countries):
 
 
 def get_record_from_file(filename):
+    """Get record from file extracting information in list of lists form."""
     records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
@@ -30,6 +33,7 @@ def get_record_from_file(filename):
 
 
 def process_records(records):
+    """Process information in list (records) passed in and create dictionary."""
     champion_to_win = {}
     countries = set()
     for record in records:
