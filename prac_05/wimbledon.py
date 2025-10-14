600c447319b52b82cdafcd6534cb16adf2b94adc
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-10-14 16:09:51 +1000

    Complete prac 5 wimbledon exercise

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,44 @@
+"""
+Wimbledon
+Estimate: 30 mins
+Actual:
+"""
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    records = get_record_from_file(FILENAME)
+    champion_to_win, countries = process_records(records)
+    print_results(champion_to_win, countries)
+
+
+def print_results(champion_to_win, countries):
+    for name, win in champion_to_win.items():
+        print(name, win)
+    print(f"These {len(countries)} countries have won Wimbledon:")
+    print(", ".join(sorted(countries)))
+
+
+def get_record_from_file(filename):
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
+def process_records(records):
+    champion_to_win = {}
+    countries = set()
+    for record in records:
+        countries.add(record[1])
+        try:
+            champion_to_win[record[2]] = champion_to_win[record[2]] + 1
+        except KeyError:
+            champion_to_win[record[2]] = 1
+    return champion_to_win, countries
+
+
+main()
