a0e2651d2ff3713d3da26ba324577d498a9af906
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-11-12 10:39:21 +1000

    Add docstrings to final solution

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,9 +7,11 @@ MILES_TO_KM = 1.60934
 
 
 class ConvertMilesKmApp(App):
+    """Kivy App for converting miles to kilometres."""
     message = StringProperty()
 
     def build(self):
+        """Build the Kivy app from kv file."""
         Window.size = (1280, 720)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
@@ -17,21 +19,25 @@ class ConvertMilesKmApp(App):
         return self.root
 
     def calculate(self):
+        """Calculate the conversion from miles to kilometres upon call."""
         number = self.get_valid_miles()
         result = number * MILES_TO_KM
         self.root.ids.km_text.text = f"{result:.2f}km"
 
     def handle_increment(self, increment_value):
+        """Handle up and down button press upon call."""
         number = self.get_valid_miles() + increment_value
         self.root.ids.input_miles.text = str(number)
         self.calculate()
 
     def get_valid_miles(self):
+        """Get a valid number to prevent program from crashing upon incorrect input."""
         try:
             number = float(self.root.ids.input_miles.text)
             return number
         except ValueError:
-            return None
+            return 0
 
 
 ConvertMilesKmApp().run()
+
