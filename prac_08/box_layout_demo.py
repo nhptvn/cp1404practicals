0e2047fafd1ff0145171ac1600fa0d41bea3d9c5
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-11-12 10:42:17 +1000

    Add docstrings to final solution

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,16 +3,20 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Kivy app greeting message."""
     def build(self):
+        """Build the Kivy app from kv file."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle the greeting message upon call."""
         # print('greet')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Handle clearing texts upon call."""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
