b19c768f2ebb95a1f1383fcbb1d84d29c90d4fe6
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-11-18 12:55:35 +1000

    Complete unreliable car exercise for prac 9

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,14 @@
+import random
+
+from prac_09.car import Car
+
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        if self.reliability > random.randint(0, 100):
+            super().drive(distance)
+        return distance
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,17 @@
+from prac_09.unreliable_car import UnreliableCar
+
+
+def main():
+    reliable_car = UnreliableCar("Toyota", 10000, 90)
+    reliable_car.drive(50)
+    unreliable_car = UnreliableCar("Nissan", 10000, 10)
+    unreliable_car.drive(50)
+    for i in range(50, 55):
+        print(f"{reliable_car.name} drove {reliable_car.drive(i)}")
+        print(f"{unreliable_car.name} drove {unreliable_car.drive(i)}")
+    print(reliable_car)  # More on the odometer means driven more which means better reliability
+    print(unreliable_car)
+
+
+main()
+
