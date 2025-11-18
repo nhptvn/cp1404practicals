80c07591d4837c6f20af1c05f0424d026d0bf6d3
tyt <huuphuocthinh.nguyen@my.jcu.edu.au>
2025-11-18 12:29:14 +1000

    Complete Test taxi and modified Taxi class for prac 9

---
+++ prac_09/taxt_test.py
@@ -0,0 +1,13 @@
+from prac_09.taxi import Taxi
+
+
+def main():
+    taxi1 = Taxi("Prius 1", 100)
+    taxi1.drive(40)
+    print(taxi1)
+    taxi1.start_fare()
+    taxi1.drive(100)
+    print(taxi1)
+
+
+main()
